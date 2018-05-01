a4f035b95859cd52039782a963b63313d1dfd64b
dmitry <dim@example.com>
2018-05-01 17:51:51 +0300

    output the result Map List

--- src/Main.java
+++ src/Main.java
@@ -9,10 +9,12 @@ import java.util.*;
  */
 public class Main {
     public static void main(String[] args) {
-        String[] randomWords = generateRandomWords(200);
+        String[] randomWords = generateRandomWords(2000);
         char letter = scanLetter();
-        List repeatingLetterWords = findRepeatingLetter(randomWords, letter);
-        System.out.println(repeatingLetterWords);
+        List repeatingLetterWordsInList = findRepeatingLetterList(randomWords, letter);
+        Map repeatingLetterWordsInMap = findRepeatingLetterMap(randomWords, letter);
+        System.out.println(repeatingLetterWordsInList);
+        System.out.println(repeatingLetterWordsInMap);
 
     }
 
@@ -20,7 +22,7 @@ public class Main {
         String[] randomStrings = new String[numberOfWords];
         Random random = new Random();
         for (int i = 0; i < numberOfWords; i++) {
-            char[] word = new char[random.nextInt(10) + 1];
+            char[] word = new char[random.nextInt(15) + 1];
             for (int j = 0; j < word.length; j++) {
                 word[j] = (char) (random.nextInt(26) + 97);
             }
@@ -29,18 +31,41 @@ public class Main {
         return randomStrings;
     }
 
-    public static List findRepeatingLetter(String[] words, char letter) {
+    private static Map findRepeatingLetterMap(String[] words, char letter) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String word : words) {
+            char[] arr = word.toCharArray();
+            Arrays.sort(arr);
+            int count = countLetter(arr, letter);
+            if (count > 2) {
+                map.put(word, count);
+            }
+        }
+        return map;
+    }
+
+    public static List findRepeatingLetterList(String[] words, char letter) {
         List list = new ArrayList();
         for (String word : words) {
             char[] arr = word.toCharArray();
             Arrays.sort(arr);
             for (int i = 0; i < arr.length - 1; i++) {
-                if (arr[i] == letter && arr[i] == arr[i + 1]) {
+                if (arr[i] == letter && arr[i] == arr[i + 1] && arr[i] == arr[i + 2]) {
                     list.add(word);
                 }
             }
         }
         return list;
+    }
+
+    private static int countLetter(char[] arr, char letter) {
+        int coutn = 0;
+        for (char c : arr) {
+            if (c == letter) {
+                coutn++;
+            }
+        }
+        return coutn;
     }
 
     public static char scanLetter() {
